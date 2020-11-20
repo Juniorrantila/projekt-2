d8b3d9f503e8aaa01d882385c7601e4a908a3387
Junior Rantila <junior.rantila@gmail.com>
2020-11-20 17:02:20 +0100

    put text ontop of slides

---
+++ custom_tag.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    bkg_img = document.getElementsByTagName("bkg-img");
+    for (let i = 0; i<bkg_img.length; i++){
+        bkg_img[i].className = "slide"
+        bkg_img[i].style.backgroundImage = "url('"+bkg_img[i].getAttribute("src")+"')";
+    }
+});
