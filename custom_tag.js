0d87f7ada3771e46d5bd898cbd8ddf65f2cb3f58
Junior Rantila <junior.rantila@gmail.com>
2021-01-03 07:03:39 +0100

    Navbar now translucent and goes to top of screen if landscape

--- custom_tag.js
+++ custom_tag.js
@@ -1,7 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let bkg_img = document.getElementsByTagName("bkg-img");
-    for (let i = 0; i<bkg_img.length; i++){
-        bkg_img[i].className = "slide";
-        bkg_img[i].style.backgroundImage = "url('"+bkg_img[i].getAttribute("src")+"')";
-    }
-});
+let bkg_img = document.getElementsByTagName("bkg-img");
+for (let i = 0; i<bkg_img.length; i++){
+    bkg_img[i].className = "bkg-img-slide";
+    bkg_img[i].style.backgroundImage = "url('"+bkg_img[i].getAttribute("src")+"')";
+}
