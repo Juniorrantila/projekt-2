ab395989d325e0bffdce28396240ac1a7faace4e
Junior Rantila <junior.rantila@gmail.com>
2021-01-03 13:17:41 +0100

    cleanup and effects

---
+++ hue.js
@@ -0,0 +1,12 @@
+const sheet = new CSSStyleSheet();
+document.adoptedStyleSheets = [sheet];
+
+window.onscroll = () => {
+    let val = Math.abs(Math.sin(window.scrollY / window.innerHeight * Math.PI));
+
+    sheet.replaceSync(
+        "bkg-img {"+
+            "filter: " + `blur(${val}pt) ` + `hue-rotate(${val}rad) ` + `saturate(${100-val*50}%) `+ " invert(10%) brightness(125%);"+
+        "}"
+    );
+}
